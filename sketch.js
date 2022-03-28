716ca0a1ba1904ef7434e5dd2bd42f3d6c602025
Isabelle Agostinelli <agostinellii@emmanuel.edu>
2022-03-28 14:52:20 -0400

    class notes

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
-function setup () {
-  // put setup code here
+// Create the character level generator with a pre trained model
+const rnn = ml5.charRNN('models/the-full-bee-movie-script/', modelLoaded);
+
+// When the model is loaded
+function modelLoaded() {
+  console.log('Model Loaded!');
 }
 
-function draw () {
-  // put drawing code here
-}
+// Generate content
+rnn.generate({ seed: 'According to all known laws of aviation, ' }, (err, results) => {
+  console.log(results);
+});
